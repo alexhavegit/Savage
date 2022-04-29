1f61f6b9a891d7cef783d38b1706b157c251515c
alexandersgr <phd.deen@gmail.com>
2022-04-29 08:56:04 +0700

    первая кривая хуйня

--- Savage/savage.c
+++ Savage/savage.c
@@ -4,12 +4,24 @@
 #include <memory.h>
 #include <tchar.h>
 #include <wingdi.h>
+#include <stdbool.h>
+#include <math.h>
+
+
 // 
 
-#define TM_TIMER_1 1
+#define TM_SCREEN_REDRAW_TIMER 1
+#define TM_TIMER_1 2
+
+#define M_PI       3.14159265358979323846
+#define M_PIdiv180 0.0174532925199
+
+
 
 HINSTANCE hInst;
 HWND hWnd;
+
+bool ready = false;
 
 BITMAPINFOHEADER bmiHeader = { 0 };
 BITMAPINFO info = { 0 };
@@ -18,13 +30,33 @@ int image_size = 800 * 450 * 3;
 char* aimp_buffer;
 HBITMAP MainBitmap;
 
+int time = 0;
 
+typedef struct
+{
+    short r;
+    short g;
+    short b;
+
+} SV_Color;
+
+typedef struct
+{
+    int x;
+    int y;
+
+} SV_Point;
+
+SV_Point p1 = { 450, 330 };
 
 
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 void PaintExperiments(HWND, HDC);
 void DisplayError(DWORD, const char*);
+void SV_SetPixel(int, int, SV_Color);
+void SV_Rotate_Point(int, int, int*, int*, int);
+
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
@@ -64,12 +96,14 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
 
-
+    SetTimer(hWnd, TM_SCREEN_REDRAW_TIMER, 33, NULL);
     SetTimer(hWnd, TM_TIMER_1, 100, NULL);
 
 
     aimp_buffer = (char*)malloc(image_size * sizeof(char));
 
+
+    
 
 
     //ads_scrbuf->avo_buffer = (void*)aimp_buffer;
@@ -143,11 +177,47 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
         PaintExperiments(hWnd, hdc);
 
-        TextOut(hdc, 10, 10, L"experiments", 11);
+        //TextOut(hdc, 10, 10, L"experiments", 11);
 
         EndPaint(hWnd, &ps);
+
+        ready = true;
     }
     break;
+
+    case WM_TIMER:
+
+        switch (wParam)
+        {
+        case TM_SCREEN_REDRAW_TIMER:
+
+            InvalidateRect(hWnd, 0, TRUE);
+
+            return 0;
+
+
+        case TM_TIMER_1:
+            //if (ready)
+            // process the 10-second timer 
+
+            if (time < 100)
+            {
+                time++;
+            }
+            else
+            {
+                time = 0;
+            }
+
+            
+
+
+            return 0;
+        }
+
+        break;
+
+
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -164,7 +234,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 void PaintExperiments(HWND hWnd, HDC hDC)
 {
 
-    memset((char*)aimp_buffer, 0, image_size * sizeof(char));
+    //memset((char*)aimp_buffer, 0, image_size * sizeof(char));
 
 
     int counter = 0;
@@ -191,15 +261,38 @@ void PaintExperiments(HWND hWnd, HDC hDC)
      */
 
 
+    /*
     int rand1 = 0;
-    rand1 = (rand() % 10000000) * 10;
+        rand1 = (rand() % 10000000) * 10;
 
     for (int i = 100; i < 1000 + rand1; i++)
     {
         aimp_buffer[i] = 150;
     }
+    */
+
+    int Ax = 0, Ay = 0;
+    int Cx = 400, Cy = 225;
+
+    Ax = 100 + time;
+    Ay = 200;
+
+    SV_Color Cc = { 255, 255, 255 };
+    SV_SetPixel(Cx, Cy, Cc);
+
+    //p1.x += time;
+   
+    p1.x = 450;
+    p1.y = 330;
+
+    int angle = 0 + time * 3.6;
 
 
+    SV_Rotate_Point(Cx, Cy, &p1.x, &p1.y, angle);
+
+    SV_Color c = { 255, 0, 0 };
+
+    SV_SetPixel(p1.x, p1.y, c);
 
 
     //int* p = (HBITMAP*)hBMP;
@@ -227,6 +320,70 @@ void DisplayError(DWORD dw, const char* szFuncName)
         NULL);
     MessageBox(NULL, buff, szFuncName, MB_OK);
 }
+
+
+void SV_SetPixel(int x, int y, SV_Color col)
+{
+    //color col = { 255,255,255 };
+    int ny = 450 - y;
+    int pointNum = 0;
+
+    pointNum = (ny * 800 + x) * 3;
+
+    aimp_buffer[pointNum] = col.b;
+    aimp_buffer[pointNum + 1] = col.g;
+    aimp_buffer[pointNum + 2] = col.r;
+}
+
+
+//void increent(int& val){    val++;}
+
+
+void SV_Rotate_Point(int CenterX, int CenterY, int  *PointX, int  *PointY, int angle)
+{
+    float lengthCA = 0, lengthAB = 0, val;
+
+    lengthCA = sqrt(pow((CenterX - *PointX), 2) + pow((CenterY - *PointY), 2));
+
+    lengthAB = lengthCA * sin(angle * M_PIdiv180);
+
+
+
+
+
+    val = M_PIdiv180; // M_PI / 180;  
+
+
+    if (*PointX > CenterX &&  *PointY > CenterY )
+    {
+        *PointX = *PointX + lengthCA * sin(angle * val);
+        *PointY = *PointY - lengthCA * sin(angle * val);
+    }
+
+    if (*PointX > CenterX && *PointY < CenterY)
+    {
+        *PointX = *PointX - lengthCA * sin(angle * val);
+        *PointY = *PointY - lengthCA * sin(angle * val);
+    }
+
+
+    if (*PointX < CenterX && *PointY < CenterY)
+    {
+        *PointX = *PointX - lengthCA * sin(angle * val);
+        *PointY = *PointY + lengthCA * sin(angle * val);
+    }
+
+    if (*PointX < CenterX && *PointY > CenterY)
+    {
+        *PointX = *PointX + lengthCA * sin(angle * val);
+        *PointY = *PointY + lengthCA * sin(angle * val);
+    }
+    
+}
+
+
+
+
 
 
 
